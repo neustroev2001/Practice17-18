71f37093bc7a7e1681d9f1e6607be6363da4fcf7
Leonid <neustroev.leonid@mail.ru>
2020-11-19 02:27:28 +0300

    Ex.17-18

---
+++ src/task17_18/main17_18.java
@@ -0,0 +1,12 @@
+package task17_18;
+
+public class main17_18 {
+    public static void main(String[] args) {
+//        First first = new First();
+//        first.check();
+//        Second second=new Second();
+//        second.check();
+        Third third = new Third();
+        third.check();
+    }
+}
